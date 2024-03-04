7036475500491b36e88ec271a9ded47edc799eea
tmnpierre <tomyn.pierre@gmail.com>
2024-03-04 16:02:02 +0100

    initial Commit

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApi.Models;
+
+namespace SimpleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<Item> items = new List<Item>
+        {
+            new Item { Id = 1, Name = "Item 1" },
+            new Item { Id = 2, Name = "Item 2" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:5008")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("MyPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
